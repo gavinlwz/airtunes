266daf572eb6bcd4dc4479a1c70fd5cd2ae3298d
Jessica Dohee Kim <jessicadoheekim@hnt.1817.wlanwireless-pennnet.upenn.edu>
2016-03-25 03:46:59 -0400

    link up

--- app/src/main/java/com/mycompany/airtunes/MainActivity.java
+++ app/src/main/java/com/mycompany/airtunes/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.firebase.client.Firebase;
@@ -78,10 +79,6 @@ public class MainActivity extends Activity implements
 
     private static final String CLIENT_SECRET = "06d91d09593e46a78ca86fe7a118d10d";
 
-
-
-
-
     public static Player mPlayer;
 
     final static String clientId = "669bf6828431431c8b5c90f729921077";
@@ -107,11 +104,6 @@ public class MainActivity extends Activity implements
     User testUser;
     Group testRoom;
 
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("Hallo", "hi Wai");
@@ -125,8 +117,6 @@ public class MainActivity extends Activity implements
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
-
-
 //
 //        // Testing code
 //        testUser = new User("Wai", "Wu", "ihugacownow");
@@ -143,13 +133,6 @@ public class MainActivity extends Activity implements
 //                }
 //            }
 //        });
-
-
-
-
-
-
-
     }
 
     @Override
@@ -159,19 +142,14 @@ public class MainActivity extends Activity implements
 
         System.out.println(resultCode);
 
-
         // Check if result comes from the correct activity
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
             if (response.getType() == AuthenticationResponse.Type.TOKEN) {
                 access_token = response.getAccessToken();
                 new RetrieveFeedTask().execute();
-
-
                 api.setAccessToken(access_token);
-
                 Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
-
                 Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
                     @Override
                     public void onInitialized(Player player) {
@@ -182,28 +160,28 @@ public class MainActivity extends Activity implements
 
                     }
 
-
                     @Override
                     public void onError(Throwable throwable) {
                         Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
                     }
                 });
             }
-
         }
+    }
 
+    public void onGoToGroupButtonClick(View view) {
+        Intent i = new Intent(getApplicationContext(), SearchGroupActivity.class);
+        startActivity(i);
+    }
 
-
-
-
+    public void onGoToProfileButtonClick(View view) {
+        Intent i = new Intent(getApplicationContext(), SearchUserActivity.class);
+        startActivity(i);
     }
 
     public static void logout(View view) {
         AuthenticationClient.clearCookies(view.getContext());
-        
     }
-
-
 
     /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -258,25 +236,14 @@ public class MainActivity extends Activity implements
         }
     }*/
 
-
-
+    @Override
+    public void onLoggedIn() { Log.d("MainActivity", "User logged in"); }
 
     @Override
-    public void onLoggedIn() {
-        Log.d("MainActivity", "User logged in");
-
-
-    }
+    public void onLoggedOut() { Log.d("MainActivity", "User logged out"); }
 
     @Override
-    public void onLoggedOut() {
-        Log.d("MainActivity", "User logged out");
-    }
-
-    @Override
-    public void onLoginFailed(Throwable error) {
-        Log.d("MainActivity", "Login failed");
-    }
+    public void onLoginFailed(Throwable error) { Log.d("MainActivity", "Login failed"); }
 
     @Override
     public void onTemporaryError() {
@@ -308,9 +275,7 @@ public class MainActivity extends Activity implements
 
         private Exception exception;
 
-        protected void onPreExecute() {
-
-        }
+        protected void onPreExecute() { }
 
         protected String doInBackground(Void... urls) {
             // Do some validation here
@@ -344,21 +309,17 @@ public class MainActivity extends Activity implements
                 System.out.println("my account type is " + accountType);
                 System.out.println("my uri is " + profilePic);
 
-                if (accountType.equals("premium")) {
-                    Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
-                    i.putExtra("fullName", fullName);
-                    i.putExtra("accountType", accountType);
-                    i.putExtra("profilePic", profilePic);
-                    startActivity(i);
-                } else {
-                    Intent i = new Intent(getApplicationContext(), PremiumRedirectActivity.class);
-                    startActivity(i);
-
-                }
-
-
-
-
+//                if (accountType.equals("premium")) {
+//                    Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
+//                    i.putExtra("fullName", fullName);
+//                    i.putExtra("accountType", accountType);
+//                    i.putExtra("profilePic", profilePic);
+//                    startActivity(i);
+//                } else {
+//                    Intent i = new Intent(getApplicationContext(), PremiumRedirectActivity.class);
+//                    startActivity(i);
+//
+//                }
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
@@ -376,15 +337,5 @@ public class MainActivity extends Activity implements
             return null;
         }
     }
-
-
-
-
-
-
-    /**
-     * Created by akashsubramanian on 3/4/16.
-     */
-
 }
 
